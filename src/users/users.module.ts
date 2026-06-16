361122aea03e0f2aca5df9599b67464f8af8fa8e
AliKhanmo <lykhanmhmdy@gmail.com>
2026-06-16 21:36:03 +0330

    fix:usersService name

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './user.schema';
-import { UserService } from './user.service';
+import { UsersService } from './users.service';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
-  providers: [UserService],
-  exports: [UserService],
+  providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule { }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Inject, Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { User, UserDocument } from "./user.schema";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectModel(User.name) private userModel: Model<UserDocument>,
+    ) { }
+    async create(userDto: { email: string, password: string }): Promise<User> {
+        const check = await this.userModel.findOne({ email: userDto.email })
+        if (check) throw new ConflictException("Email Exists");
+        return this.userModel.create(userDto);
+    }
+    async findByEmail(email: string): Promise<User | null> {
+        return await this.userModel.findOne({ email }).exec();
+    }
+    async findById(id: string): Promise<User | null> {
+        return await this.userModel.findById(id).exec();
+    }
+}
